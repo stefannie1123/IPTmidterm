e85113eeeb51582892886a739e090d7295e1ad9d
stefannie1123 <71477344+stefannie1123@users.noreply.github.com>
2021-10-24 22:40:48 +0800

    first commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,33 +9,30 @@
     </div>
       <h1 class="headertxt">Recent Post</h1>
       <hr>
-      @foreach($posts as $post)
-      @if ($post->user->gender == 'Male')
-      <div class="card m-1 mb-3 pb-3 pt-3" style="background-color: lightblue">
-      @else
-      <div class="card m-1 mb-3 pb-3 pt-3" style="background-color: lightpink">
-      @endif
-      <div class="card-body">
-        <div class="dropdown float-end">
+      @foreach($posts as $p)
+  @if ($p->user->gender == 'Male')
+  <div class="card m-1 mb-3 pb-3 pt-3" style="background-color: lightblue">
+  @else
+  <div class="card m-1 mb-3 pb-3 pt-3" style="background-color: lightpink">
+  @endif
+    <div class="card-body">
+      <div class="dropdown float-end">
         <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-          <i class="fas fa-list"></i> {{$post->category->category}}
+          <i class="fas fa-list"></i> {{$p->category->category}}
         </button>
         <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-          @foreach(App\Models\User::whereHas('posts', function($query) use ($post){
-          $query->where('category_id', $post->category_id);
-          })->get() as $user)
-          <li><a class="dropdown-item" href="/authors/{{$user->id}}">{{$user->name}}</a></li>
-          @endforeach
+            @foreach(App\Models\User::whereHas('posts', function($query) use ($p){
+                $query->where('category_id', $p->category_id);
+                })->get() as $u)
+                <li><a class="dropdown-item" href="/authors/{{$u->id}}">{{$u->name}}</a></li>
+            @endforeach
         </ul>
       </div>
-        <p class="card-title">
-        <i class="fas fa-user"></i> {{$post->user->name}}
-        </p>
-        <p class="card-text bg-white text-dark p-3" style="border-radius: 10px;">
-          {{$post->post}}
-        </p>
-      </div>
-      @endforeach
+      <h5 class="card-title pb-3"><i class="fas fa-user"></i> {{$p->user->name}}</h5>
+      <p class="card-text bg-white p-3" style="border-radius: 10px;">{{$p->post}}</p>
+    </div>
+  </div>
+  @endforeach
   </div>
 </div>
 @endsection
